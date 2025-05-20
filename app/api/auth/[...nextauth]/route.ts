3d9a5158978717d58886543ba5f3138921516297
sadpig <177748749@qq.com>
2025-05-20 18:58:16 +0800

    test1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,8 @@
 // app/api/auth/[...nextauth]/route.ts
 
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  debug: true,
-};
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
